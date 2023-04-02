bd9b1fdf44dd82d36b009848eb97e6e0f3bf158f
badgerbadgers <tim.almacen@gmail.com>
2023-04-02 15:53:36 -0700

    week05 code created new files in starter folder of 03-task-manager and new file quizAnswers.txt

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,32 @@
-console.log('Task Manager App')
+require("./db/connect")
+const express = require("express")
+const app = express()
+const tasks = require("./routes/tasks")
+const connectDB = require("./db/connect")
+//get middleware
+
+app.use(express.json())
+
+//routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App")
+})
+
+//routes
+app.use("/api/v1/tasks", tasks)
+
+//app.get('/api/v1/tasks')  - get all tasks
+//app.post('/api/v1/tasks')
+//app.get('/api/v1/tasks/:id') - get single task
+//app.patch('/api/v1/tasks/:id') - update task
+//app.delete('/api/v1/tasks/:id') - delete task
+
+const port = 5200
+const start = async () => {
+  try {
+    await connectDB()
+    app.listen(port, console.log(`Server is listening ${port}...`))
+  } catch (error) {
+    console.log(error)
+  }
+}
---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,31 @@
+const { create } = require("../models/Task")
+const Task = require("../models/Task")
+//controller function
+const getAllTasks = (req, res) => {
+  res.send("all items from file")
+}
+
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body)
+  res.status(201).json({ task })
+}
+
+const getTask = (req, res) => {
+  res.json({ id: req.params.id })
+}
+
+const updateTask = (req, res) => {
+  res.send("update task")
+}
+
+const deleteTask = (req, res) => {
+  res.send("delete task")
+}
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  updateTask,
+  getTask,
+  deleteTask,
+}
